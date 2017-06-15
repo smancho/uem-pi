3a0b6e4fc02c16394919e09b5eda69c45dd54287
Sandra Mancho <sandra.mancho@gmail.com>
2017-06-15 05:40:28 +0200

    src bbdd

--- src/bdd/Crud.java
+++ src/bdd/Crud.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public interface Crud {
 	public void create(Object newRecord) throws SQLException;
-	public ArrayList<Object> read(Object filter) throws SQLException;
+	public ArrayList<?> read(Object filter) throws SQLException;
 	public int update(Object filter, Object newData) throws SQLException;
 	public int delete(Object filter) throws SQLException;
 }
